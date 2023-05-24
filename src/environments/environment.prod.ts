6d8971a3a271c9427edc7d19f4d0f9751c050999
davidn7s <campos.magro.david.0@gmail.com>
2023-05-24 18:33:27 +0200

    Environment produccion agregado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    
+    apiKey: "AIzaSyCRdsfRw_g88iXq2O-Ci_4gBMkP4-uxo1c",
+    authDomain: "asociacionouroboros.firebaseapp.com",
+    projectId: "asociacionouroboros",
+    storageBucket: "asociacionouroboros.appspot.com",
+    messagingSenderId: "133226905340",
+    appId: "1:133226905340:web:a07a07c8efc90eef3b6179"
+  }
 };
