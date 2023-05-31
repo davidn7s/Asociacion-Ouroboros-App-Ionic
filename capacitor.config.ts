a1a06fed9845563cea706666be7f6da78343bb85
davidn7s <campos.magro.david.0@gmail.com>
2023-05-31 20:01:17 +0200

    Cambio de nombre a la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'asociacionOuroboros',
+  appName: 'Asociación Ouroboros',
   webDir: 'www',
   bundledWebRuntime: false
 };
